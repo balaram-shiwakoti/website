9abfb6b1830bebed762cce52ae96ddeb4a046fdf
Balaram Shiwakoti <prajinshiwakoti@gmail.com>
2023-01-05 20:23:18 +0545

    header and hero section

---
+++ script.js
@@ -0,0 +1,6 @@
+// const navbarToggler = document.querySelector(".navbar-toggler");
+// const navItems = document.querySelector(".nav-items");
+
+// navbarToggler.addEventListener("click", function () {
+//   navItems.classList.toggle("nav-items-visible");
+// });
